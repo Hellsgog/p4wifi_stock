4b950c40483c003eb808bc86d2df82a6b0bb7ffd
Kasper Hettinga <kasper.hettinga@gmail.com>
2014-01-02 07:48:43 +0100

    Added build option for p4 as well

---
+++ build_p4.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+
+# Build Mackay kernel for p4
+
+# Set basic parameters
+DATE_START=$(date +"%s")
+
+MACKAY_VER="Mackay_1.5_p4"
+
+export ARCH=arm
+export LOCALVERSION="-"`echo $MACKAY_VER`
+export CROSS_COMPILE=/home/kasper/android/omni/prebuilts/gcc/linux-x86/arm/arm-eabi-4.4.3/bin/arm-eabi-
+
+BASE_DIR=/home/kasper/android
+SOURCE_DIR=`echo $BASE_DIR`/p4wifi_stock
+OUTPUT_DIR=`echo $BASE_DIR`/output/galaxytab
+KERNEL_DIR=`echo $OUTPUT_DIR`/kernel
+MODULES_DIR=`echo $OUTPUT_DIR`/modules
+
+# Clean
+rm $KERNEL_DIR/zImage
+rm $OUTPUT_DIR/*.zip
+
+# Build
+cd $SOURCE_DIR
+make mackay_p4_defconfig
+make -j4
+
+# Move modules and kernel to output folder
+# rm `echo $MODULES_DIR"/*"`
+
+find $SOURCE_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+chmod 644 `echo $MODULES_DIR"/*"`
+
+cp arch/arm/boot/zImage $KERNEL_DIR
+
+# Create flashable zip
+cd $OUTPUT_DIR
+zip -r `echo $MACKAY_VER`.zip *
+cp `echo $MACKAY_VER`.zip /home/kasper/AndroidFlash/p4/
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
